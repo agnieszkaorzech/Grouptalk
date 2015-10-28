87a40b3d1ccbae03c6d1cf817381bf4506837052
PaulinaOpanowska <paulinaopanowska@gmail.com>
2015-10-28 01:10:07 +0100

    Sting resource added

---
+++ service/src/main/java/edu/upc/eetac/dsa/grouptalk/GrouptalkMediaType.java
@@ -0,0 +1,12 @@
+package edu.upc.eetac.dsa.grouptalk;
+
+/**
+ * Created by Hp on 2015-10-14.
+ */
+public class GrouptalkMediaType {
+    public final static String GROUPTALK_AUTH_TOKEN = "application/vnd.dsa.grouptalk.auth-token+json";
+    public final static String GROUPTALK_USER = "application/vnd.dsa.grouptalk.user+json";
+    public final static String GROUPTALK_STING = "application/vnd.dsa.grouptalk.sting+json";
+    public final static String GROUPTALK_STING_COLLECTION = "application/vnd.dsa.grouptalk.sting.collection+json";
+    public final static String GROUPTALK_ROOT = "application/vnd.dsa.grouptalk.root+json";
+}
